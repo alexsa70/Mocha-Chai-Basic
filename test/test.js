ada99421f048da3c75bf2a90ac202bd95dca921c
Alex Leshinsky <alexl@surfsolutions.com>
2020-01-14 19:19:14 +0200

    My first test on Mocha Chai

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('A basic test', function() {
+    it('should pass when everithing is OK', function() {
+        expect(true).to.be.true;
+    });
+});
